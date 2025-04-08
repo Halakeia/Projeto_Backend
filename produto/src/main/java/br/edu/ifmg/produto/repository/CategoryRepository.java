e34d497c555706e2229261c554a40ab74a3b8bf5
raissa <fariaferreiraraissa@gmail.com>
2025-04-07 23:47:00 -0300

    feat: implementação de requisições REST

--- produto/src/main/java/br/edu/ifmg/produto/repository/CategoryRepository.java
+++ produto/src/main/java/br/edu/ifmg/produto/repository/CategoryRepository.java
@@ -4,7 +4,10 @@ import br.edu.ifmg.produto.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository //anotacao opcional o spring já reconhece como repositorio pelo uso do JPA
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+/*
+* JpaRepository é uma interface forneccida pelo spring data que já vem com varios metodos prontos
+* para manipulacao no banco de dados*/
 
 }
--- produto/src/main/java/br/edu/ifmg/produto/services/CategoryService.java
+++ produto/src/main/java/br/edu/ifmg/produto/services/CategoryService.java
@@ -3,12 +3,15 @@ package br.edu.ifmg.produto.services;
 import br.edu.ifmg.produto.dtos.CategoryDTO;
 import br.edu.ifmg.produto.entities.Category;
 import br.edu.ifmg.produto.repository.CategoryRepository;
+import br.edu.ifmg.produto.services.exceptions.ResourceNotFound;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -23,10 +26,31 @@ public class CategoryService {
         List<Category> list = categoryRepository.findAll();
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
+
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
-       Optional<Category> obj = categoryRepository.findById(id);
-      Category category = obj.get();
-      return new CategoryDTO(category);
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category category = obj.orElseThrow(() -> new ResourceNotFound("Categoria " + id + " não encontrada")); //se não for encontrado retorna a exceção criada em exceptions
+        return new CategoryDTO(category);
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+   Category entity = new Category();
+   entity.setName(dto.getName());
+   entity = categoryRepository.save(entity); //save retorna o objeto com o id auto-incrementado
+   return new CategoryDTO(entity);
+    }
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try{
+            Category entity = categoryRepository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity = categoryRepository.save(entity);
+            return new CategoryDTO(entity);
+        }catch (EntityNotFoundException ex) {
+            throw new ResourceNotFound("Categoria não encontrada");
+        }
+
     }
 }
