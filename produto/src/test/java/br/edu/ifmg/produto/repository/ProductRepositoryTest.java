25f67ca7226efdd8931c3a90453471441747584a
raissa <fariaferreiraraissa@gmail.com>
2025-05-07 10:29:33 -0300

    Aula 10 - Camada de serviço

--- produto/src/test/java/br/edu/ifmg/produto/repository/ProductRepositoryTest.java
+++ produto/src/test/java/br/edu/ifmg/produto/repository/ProductRepositoryTest.java
@@ -1,6 +1,9 @@
 package br.edu.ifmg.produto.repository;
+
 import br.edu.ifmg.produto.entities.Product;
+import br.edu.ifmg.produto.util.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +17,36 @@ public class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
-    @Test
-    @DisplayName(value = "verificando se o objeto não existe no BD depois de deletado")
-    public void deleteShouldDeleteProduct(){
-        productRepository.deleteById(1L);
-        Optional<Product> obj = productRepository.findById(1L);
-        Assertions.assertFalse(obj.isPresent());
+    private Long existingId;
 
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
     }
+
+    @Test
+    @DisplayName(value = "Verificando se o objeto" +
+            "não existe no BD depois de deletado.")
+    public void deleteShouldDeleteObjectWhenIdExists() {
+
+        productRepository.deleteById(existingId);
+        Optional<Product> obj = productRepository.findById(existingId);
+
+        Assertions.assertFalse(obj.isPresent());
+    }
+    @Test
+    @DisplayName(value = "Verificando o autoincremento" +
+            "da chave primária.")
+    public void insertShouldPersistWithAutoincrementIdWhenIdZero() {
+        Product product = Factory.createProduct();
+        product.setId(0);
+
+        Product p = productRepository.save(product);
+        Optional<Product> obj
+                = productRepository.findById(p.getId());
+        Assertions.assertTrue(obj.isPresent());
+        Assertions.assertNotEquals(0, obj.get().getId());
+        Assertions.assertEquals(26, obj.get().getId());
+    }
+
 }
